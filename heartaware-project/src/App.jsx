bb22a87a9531609c9ad258414b78ae741af04de8
Orlando129 <orlandotelles129@gmail.com>
2024-10-08 22:44:07 -0300

    criacao da pagina analise grafica

--- heartaware-project/src/App.jsx
+++ heartaware-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home";
+import Analise from "./pages/Analise/Analise";
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/analiseGrafica" element={<Analise/>}/>
       </Routes>
       <Footer />
     </Router>
